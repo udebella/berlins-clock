816a753260cd631ac458cc7ef38803ff6145d6ae
Ubu <ubald2b@gmail.com>
2017-10-22 17:07:22 +0200

    Added acceptance test

---
+++ src/main/java/berlinsclock/BerlinClock.java
@@ -0,0 +1,9 @@
+package berlinsclock;
+
+import java.time.LocalTime;
+
+public class BerlinClock {
+    public static String format(LocalTime time) {
+        return null;
+    }
+}
---
+++ src/test/java/berlinsclock/BerlinClockFeature.java
@@ -0,0 +1,18 @@
+package berlinsclock;
+
+import org.junit.Test;
+
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BerlinClockFeature {
+    @Test
+    public void should_convert_a_digital_time_into_berlin_representation() {
+        LocalTime time = LocalTime.of(19, 42, 55);
+
+        String berlinRepresentation = BerlinClock.format(time);
+
+        assertThat(berlinRepresentation).isEqualTo("O\nRRRO\nRRRR\nYYRYYRYY\nYYOO");
+    }
+}
