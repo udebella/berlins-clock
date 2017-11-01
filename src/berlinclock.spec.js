72a9b41b11f35732b9f036fb92ef958469b76a15
Ubu <ubald2b@gmail.com>
2017-11-01 16:19:29 +0100

    [ENH] Avoid repeating chai.expect

--- src/berlinclock.spec.js
+++ src/berlinclock.spec.js
@@ -1,4 +1,4 @@
-const chai = require('chai')
+const {expect} = require('chai')
 const berlinClock = require('./berlinclock')
 
 const createTime = (hour = 0, minute = 0, seconds = 0) => new Date(1970, 1, 1, hour, minute, seconds)
@@ -8,31 +8,31 @@ describe('Berlin clock', () => {
         it('all lights should be off at midnight', () => {
             const time = createTime()
 
-            chai.expect(berlinClock.singleMinutes(time)).to.equal('OOOO')
+            expect(berlinClock.singleMinutes(time)).to.equal('OOOO')
         })
 
         it('all lights should be on at 1 sec to midnight', () => {
             const time = createTime(23, 59, 59)
 
-            chai.expect(berlinClock.singleMinutes(time)).to.equal('YYYY')
+            expect(berlinClock.singleMinutes(time)).to.equal('YYYY')
         })
 
         it('half of the lights should be on at 12:32', () => {
             const time = createTime(12, 32, 0)
 
-            chai.expect(berlinClock.singleMinutes(time)).to.equal('YYOO')
+            expect(berlinClock.singleMinutes(time)).to.equal('YYOO')
         })
 
         it('all lights should be on at 12:34', () => {
             const time = createTime(12, 34, 0)
 
-            chai.expect(berlinClock.singleMinutes(time)).to.equal('YYYY')
+            expect(berlinClock.singleMinutes(time)).to.equal('YYYY')
         })
 
         it('all lights should be off at 12:35', () => {
             const time = createTime(12, 35, 0)
 
-            chai.expect(berlinClock.singleMinutes(time)).to.equal('OOOO')
+            expect(berlinClock.singleMinutes(time)).to.equal('OOOO')
         })
     })
 
@@ -40,31 +40,31 @@ describe('Berlin clock', () => {
         it('all lights should be off at midnight', () => {
             const time = createTime()
 
-            chai.expect(berlinClock.fiveMinutes(time)).to.equal('OOOOOOOOOOO')
+            expect(berlinClock.fiveMinutes(time)).to.equal('OOOOOOOOOOO')
         })
 
         it('all lights should be on at 1 sec to midnight', () => {
             const time = createTime(23, 59, 59)
 
-            chai.expect(berlinClock.fiveMinutes(time)).to.equal('YYRYYRYYRYY')
+            expect(berlinClock.fiveMinutes(time)).to.equal('YYRYYRYYRYY')
         })
 
         it('all lights should be off at 12:04', () => {
             const time = createTime(12, 4, 0)
 
-            chai.expect(berlinClock.fiveMinutes(time)).to.equal('OOOOOOOOOOO')
+            expect(berlinClock.fiveMinutes(time)).to.equal('OOOOOOOOOOO')
         })
 
         it('some lights should be on at 12:23', () => {
             const time = createTime(12, 23, 0)
 
-            chai.expect(berlinClock.fiveMinutes(time)).to.equal('YYRYOOOOOOO')
+            expect(berlinClock.fiveMinutes(time)).to.equal('YYRYOOOOOOO')
         })
 
         it('some lights should be off at 12:35', () => {
             const time = createTime(12, 35, 0)
 
-            chai.expect(berlinClock.fiveMinutes(time)).to.equal('YYRYYRYOOOO')
+            expect(berlinClock.fiveMinutes(time)).to.equal('YYRYYRYOOOO')
         })
     })
 
@@ -72,31 +72,31 @@ describe('Berlin clock', () => {
         it('all lights should be off at midnight', () => {
             const time = createTime()
 
-            chai.expect(berlinClock.singleHours(time)).to.equal('OOOO')
+            expect(berlinClock.singleHours(time)).to.equal('OOOO')
         })
 
         it('all lights should be on at 1 sec to midnight', () => {
             const time = createTime(23, 59, 59)
 
-            chai.expect(berlinClock.singleHours(time)).to.equal('RRRO')
+            expect(berlinClock.singleHours(time)).to.equal('RRRO')
         })
 
         it('half the lights should be off at 02:04', () => {
             const time = createTime(2, 4, 0)
 
-            chai.expect(berlinClock.singleHours(time)).to.equal('RROO')
+            expect(berlinClock.singleHours(time)).to.equal('RROO')
         })
 
         it('some lights should be on at 8:23', () => {
             const time = createTime(8, 23, 0)
 
-            chai.expect(berlinClock.singleHours(time)).to.equal('RRRO')
+            expect(berlinClock.singleHours(time)).to.equal('RRRO')
         })
 
         it('all lights should be off at 14:35', () => {
             const time = createTime(14, 35, 0)
 
-            chai.expect(berlinClock.singleHours(time)).to.equal('RRRR')
+            expect(berlinClock.singleHours(time)).to.equal('RRRR')
         })
     })
 
@@ -104,31 +104,31 @@ describe('Berlin clock', () => {
         it('all lights should be off at midnight', () => {
             const time = createTime()
 
-            chai.expect(berlinClock.fiveHours(time)).to.equal('OOOO')
+            expect(berlinClock.fiveHours(time)).to.equal('OOOO')
         })
 
         it('all lights should be on at 1 sec to midnight', () => {
             const time = createTime(23, 59, 59)
 
-            chai.expect(berlinClock.fiveHours(time)).to.equal('RRRR')
+            expect(berlinClock.fiveHours(time)).to.equal('RRRR')
         })
 
         it('half the lights should be off at 02:04', () => {
             const time = createTime(2, 4, 0)
 
-            chai.expect(berlinClock.fiveHours(time)).to.equal('OOOO')
+            expect(berlinClock.fiveHours(time)).to.equal('OOOO')
         })
 
         it('some lights should be on at 8:23', () => {
             const time = createTime(8, 23, 0)
 
-            chai.expect(berlinClock.fiveHours(time)).to.equal('ROOO')
+            expect(berlinClock.fiveHours(time)).to.equal('ROOO')
         })
 
         it('all lights should be off at 16:35', () => {
             const time = createTime(16, 35, 0)
 
-            chai.expect(berlinClock.fiveHours(time)).to.equal('RRRO')
+            expect(berlinClock.fiveHours(time)).to.equal('RRRO')
         })
     })
 
@@ -136,13 +136,13 @@ describe('Berlin clock', () => {
         it('seconds light should be on at midnight', () => {
             const time = createTime()
 
-            chai.expect(berlinClock.seconds(time)).to.equal('Y')
+            expect(berlinClock.seconds(time)).to.equal('Y')
         })
 
         it('seconds light should be off at 1 sec to midnight', () => {
             const time = createTime(23, 59, 59)
 
-            chai.expect(berlinClock.seconds(time)).to.equal('O')
+            expect(berlinClock.seconds(time)).to.equal('O')
         })
     })
 
@@ -150,25 +150,25 @@ describe('Berlin clock', () => {
         it('should return right chain for midnight', () => {
             const time = createTime()
 
-            chai.expect(berlinClock.format(time)).to.equal('YOOOOOOOOOOOOOOOOOOOOOOO')
+            expect(berlinClock.format(time)).to.equal('YOOOOOOOOOOOOOOOOOOOOOOO')
         })
 
         it('should return right chain for 1 sec to midnight', () => {
             const time = createTime(23, 59, 59)
 
-            chai.expect(berlinClock.format(time)).to.equal('ORRRRRRROYYRYYRYYRYYYYYY')
+            expect(berlinClock.format(time)).to.equal('ORRRRRRROYYRYYRYYRYYYYYY')
         })
 
         it('should return right chain for 16:50:06', () => {
             const time = createTime(16, 50, 6)
 
-            chai.expect(berlinClock.format(time)).to.equal('YRRROROOOYYRYYRYYRYOOOOO')
+            expect(berlinClock.format(time)).to.equal('YRRROROOOYYRYYRYYRYOOOOO')
         })
 
         it('should return right chain for 11:37:01', () => {
             const time = createTime(11, 37, 1)
 
-            chai.expect(berlinClock.format(time)).to.equal('ORROOROOOYYRYYRYOOOOYYOO')
+            expect(berlinClock.format(time)).to.equal('ORROOROOOYYRYYRYOOOOYYOO')
         })
     })
 })
